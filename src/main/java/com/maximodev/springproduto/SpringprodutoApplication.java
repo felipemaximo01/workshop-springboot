97b9e92a90ff68105a149d2c5ff50f771f64745e
Felipe Maximo <felipecontato2001@hotmail.com>
2023-12-03 12:28:14 -0300

    projeto criado

---
+++ src/main/java/com/maximodev/springproduto/SpringprodutoApplication.java
@@ -0,0 +1,13 @@
+package com.maximodev.springproduto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringprodutoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringprodutoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/maximodev/springproduto/SpringprodutoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maximodev.springproduto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringprodutoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
