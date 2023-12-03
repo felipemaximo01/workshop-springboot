a6ae9cd846fdbef427c10d85a4ef4039eea5fa59
Felipe Maximo <felipecontato2001@hotmail.com>
2023-12-03 15:29:01 -0300

    camada de serviço e userService

--- src/main/java/com/maximodev/springproduto/resources/UserResource.java
+++ src/main/java/com/maximodev/springproduto/resources/UserResource.java
@@ -1,19 +1,33 @@
 package com.maximodev.springproduto.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.maximodev.springproduto.entities.User;
+import com.maximodev.springproduto.service.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 
+    @Autowired
+    private UserService userService;
+
     @GetMapping
-    public ResponseEntity<User> findAll(){
-        User u = new User(1L, "Felps","felps@felps", "9999999", "password");
-        return ResponseEntity.ok().body(u);
+    public ResponseEntity<List<User>> findAll(){
+        List<User> listUsers = userService.findAll();
+        return ResponseEntity.ok().body(listUsers);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id){
+        User objUser = userService.findById(id);
+        return ResponseEntity.ok().body(objUser);
     }
 }
