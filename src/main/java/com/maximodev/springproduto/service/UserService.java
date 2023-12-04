a1e9ee6d46f22d4b614bf6641cd3b3dc375aa47d
Felipe Maximo <felipecontato2001@hotmail.com>
2023-12-04 16:22:51 -0300

    user delete

--- src/main/java/com/maximodev/springproduto/service/UserService.java
+++ src/main/java/com/maximodev/springproduto/service/UserService.java
@@ -27,4 +27,8 @@ public class UserService {
     public User insert(User objUser){
         return userRepository.save(objUser);
     }
+
+    public void delete(Long id){
+        userRepository.deleteById(id);
+    }
 }
