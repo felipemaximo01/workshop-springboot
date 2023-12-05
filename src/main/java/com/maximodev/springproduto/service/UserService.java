804e4b11b149d4d2ba12f5369ecfd81832ebd3f0
Felipe Maximo <felipecontato2001@hotmail.com>
2023-12-05 20:04:15 -0300

    tratamento de erros delete

--- src/main/java/com/maximodev/springproduto/service/UserService.java
+++ src/main/java/com/maximodev/springproduto/service/UserService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.maximodev.springproduto.entities.User;
 import com.maximodev.springproduto.repositories.UserRepository;
+import com.maximodev.springproduto.service.exceptions.DatabaseException;
 import com.maximodev.springproduto.service.exceptions.ResouceNotFoundException;
 
 @Service
@@ -16,26 +19,32 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
-    public List<User> findAll(){
+    public List<User> findAll() {
         return userRepository.findAll();
     }
 
-    public User findById(Long id){
+    public User findById(Long id) {
         Optional<User> objUser = userRepository.findById(id);
         return objUser.orElseThrow(() -> new ResouceNotFoundException(id));
     }
 
-    public User insert(User objUser){
+    public User insert(User objUser) {
         return userRepository.save(objUser);
     }
 
-    public void delete(Long id){
-        userRepository.deleteById(id);
+    public void delete(Long id) {
+        try {
+            userRepository.deleteById(id);
+        } catch (EmptyResultDataAccessException e) {
+            throw new ResouceNotFoundException(id);
+        } catch(DataIntegrityViolationException e){
+            throw new DatabaseException(e.getMessage());
+        }
     }
 
-    public User update(Long id, User objUser){
+    public User update(Long id, User objUser) {
         User entity = userRepository.getReferenceById(id);
-        updateData(entity, objUser); 
+        updateData(entity, objUser);
         return userRepository.save(entity);
     }
 
